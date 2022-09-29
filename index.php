b56243b2703a135867895f60064afa983f6571cf
Denzel <denzel_silva@Torrico.com>
2022-09-29 13:24:30 -0500

    Mostrando datos y cambiando a self

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <?php
+       require_once("./conexion/conectar.php");
+       $pro = new productos();
        
     ?>
 </head>
@@ -20,9 +22,16 @@
             <th>Precio</th>
             <th>Imagen</th>
         </tr>
-        <tr>
-            
-        </tr>
+        <?php foreach(productos::mostrar() as $producto): ?>
+            <tr>
+                <td><?php echo $producto["id_producto"] ?></td>
+                <td><?php echo $producto["producto"] ?></td>
+                <td><?php echo $producto["descripcion"] ?></td>
+                <td><?php echo $producto["precio"] ?></td>
+                <td><?php echo $producto["image"] ?></td>
+            </tr>
+
+        <?php endforeach; ?>
     </table>
 </body>
 </html>
